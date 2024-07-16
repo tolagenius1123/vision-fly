f5c973725d9154617ea5918affa72f57b726bdbe
Omotola Jinadu <91120863+tolagenius1123@users.noreply.github.com>
2024-07-16 16:09:48 +0100

    fixed deployment bug

--- src/components/landing-page/Navbar.tsx
+++ src/components/landing-page/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { VisionFlyIcon } from "@/assets/icons";
+import { VisionFlyIcon } from "@/assets/icons/index";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
